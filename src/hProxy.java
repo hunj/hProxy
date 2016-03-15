7e72c174c9c3a47814aac73ff05c2f9b80c63c6a
Hun Jae Lee <hunjael@gmail.com>
2016-03-15 11:02:33 -0400

    work

--- src/hProxy.java
+++ src/hProxy.java
@@ -12,21 +12,24 @@ import java.net.*;
 public class hProxy {
 
     // program would be run in a way:
-    // $ javac proxyd.java ; java proxyd -port 1231
+    // $ javac proxyd.java
+    // $ java proxyd -port 1234
     public static void main(String[] args) throws IOException {
 
         // the default port to use if no port is specified
         int port = 5016;
 
-        System.out.println("hProxy v1.0 initiated...");
+        System.out.println("<SYSTEM>: hProxy v1.0 initiated...");
 
         // check argument and start at given port or default port, or give error if arguments are invalid.
         if (checkArguments(args)) {
             try {
                 System.out.println("<SYSTEM>: Establishing connection on hProxy on port " + args[1]);
+
+                // parsing Int from a string might throw exception if user puts weird input?
                 port = Integer.parseInt(args[1]);
             }
-            catch (Exception e) {
+            catch (Exception e) { // gotta catch 'em all
                 System.err.println("< ERR! >: please provide correct integer for port number, i.e. -port 1234");
                 e.printStackTrace();
             }
@@ -34,7 +37,7 @@ public class hProxy {
             System.out.println("<SYSTEM>: port number not given; defaulting to 5016...");
         }
 
-        // finally, start running.
+        // finally, start running at given port
         startRunningAtPort(port);
     }
 
@@ -43,7 +46,6 @@ public class hProxy {
      * checks the arguments
      * @param argumentArray the given arguments to the program
      * @return whether the arguments are valid or not
-     * @throws Exception when argument is weird?
      */
     private static boolean checkArguments(String[] argumentArray) {
         return argumentArray.length > 0 && (argumentArray[0].equals("-port") || argumentArray[0].equals("-p"));
@@ -52,23 +54,22 @@ public class hProxy {
     /**
      * Starts the proxy on given port.
      * @param portNumber the given port number to use
-     * @throws Excpetion when something is wrong
+     * @throws IOException when sum ting wong
      */
     private static void startRunningAtPort(int portNumber) {
-
         // give cute welcome message to user
         System.out.println("<SYSTEM>: Welcome to hProxy!");
         System.out.println("<SYSTEM>: Socket trying on port " + portNumber + "...");
 
-        // empty socket instance to run
-        ServerSocket hSocket = null;
         boolean keepRunning = true;
 
         // make a new server socket
         try {
-            hSocket = new ServerSocket(portNumber);
+            ServerSocket hSocket = new ServerSocket(portNumber);
             System.out.println("<SYSTEM>: Socket is now running on port " + portNumber + ".");
             System.out.println("<SYSTEM>: hProxy ready to serve.");
+
+            // here is the sweet spot
             while (keepRunning) {
                 new hProxyThread(hSocket.accept()).start();
             }
@@ -76,8 +77,7 @@ public class hProxy {
             // when done, safely close the socket.
             hSocket.close();
 
-            // some error catching
-        } catch (IOException e) {
+        } catch (IOException e) { // gotta catch 'em all
             System.err.println("< ERR! >: Could not listen on port " + portNumber);
         }
 
--- src/hProxyThread.java
+++ src/hProxyThread.java
@@ -9,7 +9,7 @@ public class hProxyThread extends Thread {
     private static final int BUFFER_SIZE = 4096;
 
     /**
-     * initializer for hProxyThread.
+     * initializer for an instance of hProxyThread.
      * @param socket the socket to use for this thread.
      */
     hProxyThread(Socket socket) {
@@ -22,9 +22,11 @@ public class hProxyThread extends Thread {
      */
     public void run() {
         try {
+            // input and output streams to read in the data
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            // some setup
             String inputLine;
             int count = 0;
             String requestedURL = "";
@@ -36,16 +38,19 @@ public class hProxyThread extends Thread {
                     StringTokenizer token = new StringTokenizer(inputLine);
                     token.nextToken();
                 } catch (Exception e) {
+                    // give cute notification to user
+                    System.out.println("<SYSTEM>: No more lines to read");
                     break;
                 }
 
                 if (count == 0) {
+                    // token is probably in the format of:
+                    // GET http://www.case.edu/eecs/eecs325.html ....
+                    // so we need to take out the request URL
                     String[] tokens = inputLine.split(" ");
                     requestedURL = tokens[1];
-
                     System.out.println("<SYSTEM>: Request for: " + requestedURL);
-                }
-                else {
+                } else {
                     httpRequest.add(inputLine);
                 }
                 System.out.println(inputLine);
@@ -53,9 +58,9 @@ public class hProxyThread extends Thread {
             }
 
 
-
-            String hostname = "www.case.edu";
-            String directory = "/";
+            // default/example
+            String hostname = "eecslinab1.engineering.cwru.edu";
+            String directory = ":hxl224/project1/";
 
             try {
                 // strip "http://"
@@ -68,27 +73,32 @@ public class hProxyThread extends Thread {
                 // the requested file:
                 directory = domain.substring(count, domain.length());
 
-                System.out.println(hostname);
+                System.out.println("<SYSTEM>: Socket for " + hostname);
                 System.out.println(directory);
             } catch (Exception e) {
                 System.out.println("< ERR! >: URL \"" + requestedURL + "\" cannot be processed.");
-                System.out.println("< ERR! >: Correct format is: http://www.case.edu/eecs/cs/eecs325.html");
             }
 
             System.out.println("<SYSTEM>: Creating socket for " + hostname);
 
-            // start connection with URL
+            // Here starts the sweet part
+            // start connection with URL on port 80
+            // *HTTPS (port 443) not supported
             Socket connectionSocket = new Socket(hostname, 80);
             System.out.println("<SYSTEM>: Socket successfully created.");
 
-
+            // server sends GET request to host, using given directory
             PrintWriter server = new PrintWriter(new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream())));
             System.out.println("<SYSTEM>: Sending GET request to " + requestedURL);
-            server.println("GET " + directory + " HTTP/1.1");
+            server.println("<SYSTEM>: GET " + directory + " HTTP/1.1");
             for (int i = 0; i < httpRequest.size(); i++) {
                 server.println(httpRequest.get(i));
             }
+
+            // tell me what we have
             server.println();
+
+            // flush out
             server.flush();
 
             // read from the URL into this stream
@@ -101,6 +111,7 @@ public class hProxyThread extends Thread {
                 check = is.read(data, 0, BUFFER_SIZE);
             }
 
+            // safely close out everything
             connectionSocket.close();
             input.close();
             output.close();
@@ -109,6 +120,7 @@ public class hProxyThread extends Thread {
 
         }
         catch (Exception e) {
+            // gotta catch em all
             System.err.println("< ERR! >: Error occurred:" + e.getMessage());
         }
     }
